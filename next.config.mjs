ff0311c16ebfdf0e15e280f10bff39e4c422625a
David D Bounsnana <deivosx@gmail.com>
2024-10-20 22:33:26 -0400

    fixing the pipeline errors

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,14 @@ const nextConfig = {
   },
   basePath: '/Tech-Congo-site',
   assetPrefix: '/Tech-Congo-site',
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
+  // Exclude the functions directory from the build
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
 }
 
 export default nextConfig
